a929411c46e82f78dd735dad10d0af5dd5a22f0f
Samuel Bezerra <samuelbezerrab@gmail.com>
2024-12-21 08:45:17 -0300

    wip: finish

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const BibleScraper = require('bible-scraper');
+const BibleScraper = require('bible-scraper')
 const fs = require('node:fs')
 
 const BIBLE_ID = 1608
-// const BIBLE_ID = BibleScraper.TRANSLATIONS.VULG
+// const BIBLE_ID = BibleScraper.TRANSLATIONS.VULG;
 const BIBLE_BOOK = 'GEN'
-const QUANTITY_OF_CHAPTERS = 3
+const QUANTITY_OF_CHAPTERS = 50
 
-( async () => {
+;( async () => {
 
     const Bible = new BibleScraper(BIBLE_ID)
 
     let chapters = []
-    for (const i = 1; i <= QUANTITY_OF_CHAPTERS; i++) {
-        const chapter = await Bible.chapter(BIBLE_BOOK + i)
+    for (let i = 1; i <= QUANTITY_OF_CHAPTERS; i++) {
+        const chapter = await Bible.chapter(BIBLE_BOOK + '.' + i)
         chapters.push(chapter)
     }
-    fs.writeFileSync(BIBLE_BOOK + '-', chapters)
+    fs.writeFileSync('./books/'+ BIBLE_BOOK + '-' + BIBLE_ID, JSON.stringify(chapters))
 })()
